fc140348f49a22c4a6b44051352f6ce79190a4de
ctmckay27 <ctmckay27@gmail.com>
2026-09-23 04:16:01 -1000

    Complete CPC v2 package

---
+++ omega_voice/conversational_performance_compiler_v2/__init__.py
@@ -0,0 +1,6 @@
+from .runtime import (
+    STATE_ID,SCHEMA,RECEIPT_SCHEMA,
+    compile_native_performance_v2,verify_plan_v2,
+    materialize_packets_v2,render_native_performance_v2,
+)
+__all__=["STATE_ID","SCHEMA","RECEIPT_SCHEMA","compile_native_performance_v2","verify_plan_v2","materialize_packets_v2","render_native_performance_v2"]
